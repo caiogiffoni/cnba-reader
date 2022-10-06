be4c181177b186fdbe9a9ebe01c22dd506b60a4b
Caio Giffoni <caio_cgf@hotmail.com>
2022-10-06 16:20:45 -0300

    :sparkles: data reader formatting

--- reader/views.py
+++ reader/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from rest_framework.views import APIView, Request, Response, status
+from transactions.models import Transaction
 
 from reader.serializers import ReaderSerializer
 
@@ -16,7 +17,7 @@ class HomeView(APIView):
 
         # ipdb.set_trace()
 
-        serializer.save()
+        # serializer.save()
         form = UploadFileForm(request.POST, request.FILES)
         # form = CNBAForm(request.POST, request.FILES)
         # check whether it's valid:
@@ -25,18 +26,32 @@ class HomeView(APIView):
             # with open(request.FILES["file"], "r") as file:
             # for line in request.FILES["file"]:
             for line in request.FILES["file"]:
-                # import ipdb
-
-                # ipdb.set_trace()
                 text = (
                     line.decode("unicode_escape")
                     .encode("latin1")
                     .decode("utf8")
                 )
-                tipo = text[0:1]
-                Transaction
-                serializer = ReaderSerializer(data=request.data)
-                serializer.is_valid(raise_exception=True)
+                tipo = Transaction.objects.get(tipo=text[0:1])
+
+                print(tipo)
+                data = {
+                    "tipo": tipo,
+                    "data": f"{text[1:5]}/{text[5:7]}/{text[7:9]}",
+                    "valor": float(text[9:19]) / 100,
+                    "cpf": text[19:30],
+                    "cartao": text[30:42],
+                    "hora": f"{text[42:44]}:{text[44:46]}:{text[46:48]}",
+                    "dono_da_loja": text[48:62].strip(),
+                    "nome_da_loja": text[62:81].strip(),
+                }
+                import ipdb
+
+                ipdb.set_trace()
+                # serializer = ReaderSerializer(data={
+                #     tipo:tipo,
+                #     data=text[2:8]
+                # })
+                # serializer.is_valid(raise_exception=True)
 
                 print(text)
 
--- transactions/models.py
+++ transactions/models.py
@@ -8,7 +8,7 @@ from django.db import models
 
 class Transaction(models.Model):
     id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
-    tipo = models.SmallIntegerField()
+    tipo = models.SmallIntegerField(unique=True)
     descrição = models.CharField(max_length=127)
     natureza = models.CharField(max_length=127)
     sinal = models.IntegerField()
