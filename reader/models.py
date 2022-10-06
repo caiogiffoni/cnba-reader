d8fcd4f689fc5227cc8cb455133a7fa95790c4a8
Caio Giffoni <caio_cgf@hotmail.com>
2022-10-06 12:53:34 -0300

    :sparkles: created: reader table n model

--- reader/models.py
+++ reader/models.py
@@ -1,3 +1,22 @@
+from uuid import uuid4
+
 from django.db import models
+from transactions.models import Transation
 
 # Create your models here.
+
+
+class Reader(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
+    tipo = models.ForeignKey(
+        Transation,
+        on_delete=models.CASCADE,
+        related_name="transactions",
+    )
+    data = models.DateField()
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
+    cpf = models.CharField(max_length=11)
+    cartão = models.CharField(max_length=12)
+    hora = models.TimeField()
+    dono_da_loja = models.CharField(max_length=127)
+    nome_da_loja = models.CharField(max_length=127)
