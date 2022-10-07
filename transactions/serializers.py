d7becdab1f6bf93f8307089ab045b6c4ce756222
Caio Giffoni <caio_cgf@hotmail.com>
2022-10-07 12:58:23 -0300

    :hammer: unique validatrion on transaction tipo

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from transactions.models import Transaction
 
@@ -7,3 +8,21 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = "__all__"
+
+        extra_kwargs = {
+            "tipo": {
+                "validators": [
+                    UniqueValidator(
+                        queryset=Transaction.objects.all(),
+                        message="This tipo number already exists",
+                    )
+                ]
+            }
+        }
+
+    # def validate_sinal(self, sinal):   VALIDATION TO BE IMPLEMENTED AFTER
+    #     if sinal == 0:
+    #         raise serializers.ValidationError(
+    #             "The sinal field must be -1 or +1 digits"
+    #         )
+    #     return sinal
